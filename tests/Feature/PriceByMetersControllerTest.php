86aa7ed5012d97853d1c3fb1c0f1fb366cff7f34
grosas <grosas@better.com.mx>
2022-06-06 05:21:06 -0500

    Initial Commit

---
+++ tests/Feature/PriceByMetersControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Http\Controllers;
+
+use App\Http\Controllers\PriceByMetersController;
+use PHPUnit\Framework\TestCase;
+
+class PriceByMetersControllerTest extends TestCase
+{
+
+    public function testGetByZipAggregateType()
+    {
+
+    }
+}
